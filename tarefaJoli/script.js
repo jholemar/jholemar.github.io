a9fefd62fd4d9e6b1467551bb017a1ba8c5554a5
jholemar <adonaisousa52@gmail.com>
2025-07-16 18:10:59 -0400

    teste

--- tarefaJoli/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.header-toggle').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.querySelector('.header-navagation').classList.toggle('active');
-});
